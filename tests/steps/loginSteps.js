b5379bd90317002d789da110d8d4bb2bf0de2605
mrkishmr <mrkishan1209@gmail.com>
2026-01-06 22:28:35 +0530

    Implemented Login Functionality and relevant Scenarios

--- tests/steps/loginSteps.js
+++ tests/steps/loginSteps.js
@@ -1,5 +1,4 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
-const { expect } = require('@playwright/test');
 
 const LoginPage = require('../pages/loginPage');
 const DashboardPage = require('../pages/dashboardPage');
@@ -14,53 +13,57 @@ Given('user is on the login page', async function () {
 
 When('the user logs in with valid credentials', async function () {
   await loginPage.login(
-    process.env.USERNAME,
-    process.env.PASSWORD
-  )}
-);
-
-Then('user should be logged in and must redirect to the dashboard page', async function () {
-  dashboardPage = new DashboardPage(this.page);
-  expect(await dashboardPage.isDashboardVisible()).toBeTruthy();
+    process.env.VALID_USERNAME,
+    process.env.VALID_PASSWORD
+  );
 });
+
+Then(
+  'user should be logged in and must redirect to the dashboard page',
+  async function () {
+    dashboardPage = new DashboardPage(this.page);
+    await dashboardPage.waitForDashboard();
+  }
+);
 
 When('the user tries to login with invalid credentials', async function () {
   await loginPage.login(
     process.env.INVALID_USERNAME,
     process.env.INVALID_PASSWORD
-  )}
-);
-
-Then('An Error Message must be displayed on the screen', async function () {
-  expect(await loginPage.isErrorVisible()).toBeTruthy();
+  );
 });
+
+Then(
+  'An Error Message must be displayed on the screen',
+  async function () {
+    await loginPage.waitForErrorMessage();
+  }
+);
 
 When('the user logs in with only username', async function () {
   await loginPage.login(
-    process.env.USERNAME,
+    process.env.VALID_USERNAME,
     ''
-  )}
-);
+  );
+});
 
 Then(
   'a required field message must be displayed under username field',
   async function () {
-    await expect(this.usernameRequiredMessage).toBeVisible();
-    await expect(this.usernameRequiredMessage).toHaveText('Required');
+    await loginPage.waitForUsernameRequiredMessage();
   }
 );
 
 When('the user logs in with only password', async function () {
   await loginPage.login(
     '',
-    process.env.PASSWORD
-  )}
-);
+    process.env.VALID_PASSWORD
+  );
+});
 
 Then(
   'a required field message must be displayed under password field',
   async function () {
-    await expect(this.passwordRequiredMessage).toBeVisible();
-    await expect(this.passwordRequiredMessage).toHaveText('Required');
+    await loginPage.waitForPasswordRequiredMessage();
   }
 );
--- tests/support/hooks.js
+++ tests/support/hooks.js
@@ -3,18 +3,24 @@ const { chromium, firefox, webkit } = require('playwright');
 
 Before(async function () {
   const browserName = process.env.BROWSER || 'chromium';
+  const isCI = process.env.CI === 'true';
+
+  const launchOptions = {
+    headless: isCI,   // headless in CI, headed locally
+    slowMo: isCI ? 0 : 50 // optional: slow down locally
+  };
 
   if (browserName === 'edge') {
     this.browser = await chromium.launch({
       channel: 'msedge',
-      headless: true
+      ...launchOptions
     });
   } else if (browserName === 'firefox') {
-    this.browser = await firefox.launch({ headless: true });
+    this.browser = await firefox.launch(launchOptions);
   } else if (browserName === 'webkit') {
-    this.browser = await webkit.launch({ headless: true });
+    this.browser = await webkit.launch(launchOptions);
   } else {
-    this.browser = await chromium.launch({ headless: true });
+    this.browser = await chromium.launch(launchOptions);
   }
 
   this.context = await this.browser.newContext();
